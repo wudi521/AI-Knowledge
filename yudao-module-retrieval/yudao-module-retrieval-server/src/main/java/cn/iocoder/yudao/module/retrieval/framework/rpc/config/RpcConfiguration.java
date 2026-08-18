7ea6299d4f77971f51ca1a7a7e86ba7b6462a73a
andy <your_new_email@example.com>
2026-08-18 14:01:08 +0800

    feat(retrieval): 服务依赖接线(ES/Milvus/3模块Feign) + RPC注册

--- yudao-module-retrieval/yudao-module-retrieval-server/src/main/java/cn/iocoder/yudao/module/retrieval/framework/rpc/config/RpcConfiguration.java
+++ yudao-module-retrieval/yudao-module-retrieval-server/src/main/java/cn/iocoder/yudao/module/retrieval/framework/rpc/config/RpcConfiguration.java
@@ -1,10 +1,12 @@
 package cn.iocoder.yudao.module.retrieval.framework.rpc.config;
 
-import cn.iocoder.yudao.module.retrieval.api.RetrievalApi;
+import cn.iocoder.yudao.module.ingestion.api.IngestionApi;
+import cn.iocoder.yudao.module.knowledge.api.KnowledgeApi;
+import cn.iocoder.yudao.module.model.api.ModelApi;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(value = "retrievalRpcConfiguration", proxyBeanMethods = false)
-@EnableFeignClients(clients = {RetrievalApi.class})
+@EnableFeignClients(clients = {ModelApi.class, KnowledgeApi.class, IngestionApi.class})
 public class RpcConfiguration {
 }
