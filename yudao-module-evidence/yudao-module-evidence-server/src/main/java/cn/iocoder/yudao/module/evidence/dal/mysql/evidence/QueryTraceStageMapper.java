83a88d8ab592f6b69edd93a089312466497459bc
wudi521 <45662664+wudi521@users.noreply.github.com>
2026-08-25 20:29:54 +0800

    feat: add query trace stage mapper

---
+++ yudao-module-evidence/yudao-module-evidence-server/src/main/java/cn/iocoder/yudao/module/evidence/dal/mysql/evidence/QueryTraceStageMapper.java
@@ -0,0 +1,9 @@
+package cn.iocoder.yudao.module.evidence.dal.mysql.evidence;
+
+import cn.iocoder.yudao.framework.mybatis.core.mapper.BaseMapperX;
+import cn.iocoder.yudao.module.evidence.dal.dataobject.evidence.QueryTraceStageDO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface QueryTraceStageMapper extends BaseMapperX<QueryTraceStageDO> {
+}
